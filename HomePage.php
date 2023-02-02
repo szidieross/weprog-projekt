6cd519899e45074b6c6548fd31be7c5a6c0c30f3
szidieross <szidieross@gmail.com>
2023-02-02 12:16:06 +0200

    created app

---
+++ HomePage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/styles/homepage.css">
+</head>
+
+<body>
+    <div class="container">hjghjghjghj</div>
+    <div class="categories">
+        <div class="groups card">
+            <div class="group-img"></div>
+            <div class="group-data">
+                <h2 class="group-name">Patients</h2>
+                <a href="login.php" class="btn btn-light">Log in</a>
+            </div>
+        </div>
+        <div class="groups card">
+            <div class="group-img"></div>
+            <div class="group-data">
+                <h2 class="group-name">Doctors</h2>
+                <a href="login.php" class="btn btn-light">Log in</a>
+            </div>
+        </div>
+        <div class="groups card">
+            <div class="group-img"></div>
+            <div class="group-data">
+                <h2 class="group-name">Admin</h2>
+                <a href="login.php" class="btn btn-light">Log in</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
