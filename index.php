ca90316c1d8a8d2da4803871747d6df64868178c
szidieross <szidieross@gmail.com>
2023-02-02 13:00:20 +0200

    starter components

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/styles/homepage.css">
+</head>
+
+<body >
+    <!-- <div class="image-holder">
+            <img class="main-img" src="./assets/img/header.png" alt="">
+        </div> -->
+    <div class="categories">
+        <div class="groups">
+            <div class="group-img"></div>
+            <div class="group-data">
+                <h2 class="group-name">Patients</h2>
+                <a href="login.php" class="btn btn-light">Log in</a>
+            </div>
+        </div>
+        <div class="groups">
+            <div class="group-img"></div>
+            <div class="group-data">
+                <h2 class="group-name">Doctors</h2>
+                <a href="login.php" class="btn btn-light">Log in</a>
+            </div>
+        </div>
+        <div class="groups">
+            <div class="group-img"></div>
+            <div class="group-data">
+                <h2 class="group-name">Admin</h2>
+                <a href="login.php" class="btn btn-light">Log in</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
