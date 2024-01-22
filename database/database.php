427c845dc51b6391b69d8016c73b611e54b6d771
szidieross <szidieross@gmail.com>
2024-01-22 21:00:57 +0200

    db

--- database/database.php
+++ database/database.php
@@ -1,6 +1,99 @@
-<?php
+DROP DATABASE IF EXISTS rendelo;
+
+CREATE DATABASE IF NOT EXISTS rendelo;
+
+USE rendelo;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    `user_id` int(10) NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(100) NOT NULL,
+    `last_name` varchar(100) NOT NULL,
+    `username` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    `password` varchar(100) NOT NULL,
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`user_id`)
+);
+
+INSERT INTO users (
+    `first_name`,
+    `last_name`,
+    `username`,
+    `email`,
+    `password`
+)
+VALUES
+    ('Anna', 'Kis', 'kisanna', 'kis.anna@gmail.com', '123456789'),
+    ('Adrienn', 'Nagy', 'nagyadri', 'nagy.adri@gmail.com', '123456789'),
+    ('Hanna', 'Kiss', 'kisshanna', 'kiss.hanna@gmail.com', '123456789'),
+    ('Imola', 'Ferencz', 'imolaferencz', 'imolaferencz@gmail.com', '123456789'),
+    ('Hajnalka', 'Erdely', 'erdelyhajni', 'erdelyhajni@gmail.com', '123456789'),
+    ('Mate', 'Nagy', 'nagymate', 'nagymate@gmail.com', '123456789'),
+    ('Tamas', 'Ballo', 'ballotamas', 'ballotamas@gmail.com', '123456789');
+
+DROP TABLE IF EXISTS appointments;
+
+CREATE TABLE appointments(
+    `appointment_id` int(10) NOT NULL AUTO_INCREMENT,
+    `user_id` int(10),
+    `date` date NOT NULL,
+    `time` time NOT NULL,
+    `taken` boolean NOT NULL,
+    PRIMARY KEY (`appointment_id`),
+    FOREIGN KEY (`user_id`) REFERENCES users(`user_id`)
+);
+
+INSERT INTO appointments (`date`, `time`, `taken`)
+VALUES
+    ('2023-02-06', '08:00:00', false),
+    ('2023-02-06', '09:00:00', false),
+    ('2023-02-06', '10:00:00', false),
+    ('2023-02-06', '11:00:00', false),
+    ('2023-02-06', '12:00:00', false),
+    ('2023-02-06', '13:00:00', false),
+    ('2023-02-06', '14:00:00', false),
+    ('2023-02-06', '15:00:00', false),
+    ('2023-02-06', '16:00:00', false),
+    ('2023-02-06', '17:00:00', false),
+    ('2023-02-07', '08:00:00', false),
+    ('2023-02-07', '09:00:00', false),
+    ('2023-02-07', '10:00:00', false),
+    ('2023-02-07', '11:00:00', false),
+    ('2023-02-07', '12:00:00', false),
+    ('2023-02-07', '13:00:00', false),
+    ('2023-02-07', '14:00:00', false),
+    ('2023-02-07', '15:00:00', false),
+    ('2023-02-07', '16:00:00', false),
+    ('2023-02-07', '17:00:00', false),
+    ('2023-02-07', '08:00:00', false),
+    ('2023-02-08', '09:00:00', false),
+    ('2023-02-08', '10:00:00', false),
+    ('2023-02-08', '11:00:00', false),
+    ('2023-02-08', '12:00:00', false),
+    ('2023-02-08', '13:00:00', false),
+    ('2023-02-08', '14:00:00', false),
+    ('2023-02-08', '15:00:00', false),
+    ('2023-02-08', '16:00:00', false),
+    ('2023-02-08', '17:00:00', false);
+
+UPDATE appointments SET user_id = 1, taken = true WHERE appointment_id IN (1, 2, 3, 4, 5);
+
+
+
+
+
+
+
+
+
+
+<!-- <?php
 
 ?>
+
+
 
 
 DROP DATABASE IF EXISTS rendelo;
@@ -223,4 +316,4 @@ WHERE
 --         'benedekistvan@gmail.com',
 --         '123456789',
 --         'urulógus'
---     );
+--     ); -->
